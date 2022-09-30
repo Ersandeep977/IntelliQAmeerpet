e39378307f304f192d46223ffcca467c9ddd1879
Sandeep <patelsandeep88@gmail.com>
2022-09-30 12:46:49 +0530

    add read.sh file

---
+++ Git_and_Hub/date30-09-22/read.sh
@@ -0,0 +1,21 @@
+#! bin/bash
+# this is the shell program for the open the f.txt file and show the file content
+
+echo ""
+tput setaf 6
+echo "program will be start .......!!"
+tput sgr0
+echo
+
+# code on this program
+###################################################
+while true;
+do
+   cat f.txt
+   echo ""
+   echo "refresh the page.......!!"
+   echo ""
+sleep 2
+done
+
+###################################################
