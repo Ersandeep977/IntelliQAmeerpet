9c4aaf23f5f60fe3e11b27b251a9cd052fac2294
Sandeep Kumar Patel <78929192+Ersandeep977@users.noreply.github.com>
2022-10-17 11:02:50 +0530

    Update jenking.sh

--- jenking.sh
+++ jenking.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
+# This file create by "Sandeep Kumar Patel"
 
+############
+# Block -1
 #################################################################################################
-# this block for the 
+# this block for the install jenking for normal way
 
 sudo apt-get update 
 sudo apt-get install -y openjdk-11-jdk
@@ -9,8 +12,8 @@ sudo apt-get install -y git maven
 wget https://get.jenkins.io/war-stable/2.332.1/jenkins.war
 java -jar jenkins.war
 
-
-
+###############
+# Block -2
 ###################################################################################################
 # this program file install jenking for permanent
 
@@ -22,3 +25,5 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]   https://pkg.jenki
 sudo apt-get update
 sudo apt-get install jenkins
 sudo systemctl start jenkins
+
+##########################################################################################################################################################################
