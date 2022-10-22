eef55d6bb9fd7340a7c917b909c4a8ba6f899aa1
Sandeep Kumar Patel <78929192+Ersandeep977@users.noreply.github.com>
2022-10-22 16:46:46 +0530

    Update jenking.sh

--- jenking.sh
+++ jenking.sh
@@ -1,10 +1,11 @@
-#! /bin/bash
 # This file create by "Sandeep Kumar Patel"
 
 ############
 # Block -1
 #################################################################################################
+#! /bin/bash
 # this block for the install jenking for normal way
+# this Program file Create by Sandeep Kumar Patel
 
 sudo apt-get update 
 sudo apt-get install -y openjdk-11-jdk
@@ -12,11 +13,15 @@ sudo apt-get install -y git maven
 wget https://get.jenkins.io/war-stable/2.332.1/jenkins.war
 java -jar jenkins.war
 
+
 ###############
 # Block -2
 ###################################################################################################
-# this program file install jenking for permanent
 #! /bin/bash
+# this program file install jenking for permanent
+# this Program file Create by Sandeep Kumar Patel
+# code start
+
 sudo apt-get update 
 sudo apt-get install -y openjdk-11-jdk
 sudo apt-get install -y git maven
@@ -26,4 +31,40 @@ sudo apt-get update
 sudo apt-get install -y jenkins
 sudo systemctl start jenkins
 
-##########################################################################################################################################################################
+# code End
+
+#####################################################################################################################################################################
+# install Tomcat9
+############################
+#! /bin/bash
+# this program file install Tomcat Server for permanent
+# this Program file Create by Sandeep Kumar Patel
+# code start
+
+sudo apt-get update 
+sudo apt-get install -y tomcat9
+sudo apt-get install -y tomcat9-admin
+sudo apt-get update
+
+*Edit the tomcat-users.xml file
+
+cd /etc/tomcat
+sudo vim tomcat-users.xml
+  
+Delete all the content and add the below content
+  
+<tomcat-users>
+<user username="intelliqit" password="intelliqit" roles="manager-script"/> 
+</tomcat-users>
+
+sudo service tomcat9 restart
+
+######################################################################################################################################################################
+
+
+
+
+
+
+
+
