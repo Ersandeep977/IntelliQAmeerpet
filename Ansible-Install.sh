3af7c67bbccecf71438a2a544a35198e058c3c57
Sandeep Kumar Patel <78929192+Ersandeep977@users.noreply.github.com>
2022-10-25 10:48:07 +0530

    Update Ansible-Install.sh

--- Ansible-Install.sh
+++ Ansible-Install.sh
@@ -6,3 +6,17 @@ sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt update
 sudo apt install ansible -y
+
+
+# Ansible command
+
+# to ping the all server working fine or not
+ansible all -m ping -v
+
+# run simple ls -al command
+ansible all -a "ls -al"
+
+# run ansible module 
+ansible all -m shell -a "ls -al"
+
+
