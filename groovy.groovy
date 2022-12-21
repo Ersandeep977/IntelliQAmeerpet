25b798b9b4f2998dc51fd728821f7515906e879f
Sandeep Kumar Patel <78929192+Ersandeep977@users.noreply.github.com>
2022-12-21 20:40:16 +0530

    Update groovy.groovy

--- groovy.groovy
+++ groovy.groovy
@@ -1,3 +1,4 @@
+//data type
 int a = 10
 byte b = 20
 float c = 30
@@ -7,12 +8,14 @@ char e = 'e'
 println(a)
 println(b)
 
+//opartion
 println 1+2
 println 2-1
 println 1*2
 println 1/2
 println 1%2
 
+// If Condition
 data =10
 if(data%2 == 0) {
     println "Even"
@@ -20,35 +23,44 @@ if(data%2 == 0) {
     println 'Odd'
 }
 
+// other one
 data = 11
 println data/2
 
-
+// string interoption
 println "java Home"+"hello"
 println "java Home hello"-"hello"
 
+// for loop in groovy
+// 1st
 for (int i; i<10; i++){
     println "sandeep-${i}"
 }
 
-
+// 2nd
 for (int i=0; i<10; i++){
     println "sandeep-${i+1}"
 }
 
+// 3th
 def data = ["apple","grapes","Banana"]
 data.each{
     println it
 }
+
+//4th
 def data = ["apple","grapes","Banana"]
 data.each{   x->
     println "food name is ${x}"
 }
+
+//5th
 def data = ["apple","grapes","Banana"]
 for (x in data){
     println("fruit name is ${x}")
 }
 
+// 6th time function
 10.times{
     println "java Home ${it}"
 }
