7b373145fc8f0b3da3f0d9164531b71308c96eaa
Sandeep Kumar Patel <78929192+Ersandeep977@users.noreply.github.com>
2022-12-21 19:14:39 +0530

    Update groovy.groovy

--- groovy.groovy
+++ groovy.groovy
@@ -19,3 +19,37 @@ if(data%2 == 0) {
 } else {
     println 'Odd'
 }
+
+data = 11
+println data/2
+
+
+println "java Home"+"hello"
+println "java Home hello"-"hello"
+
+for (int i; i<10; i++){
+    println "sandeep-${i}"
+}
+
+
+for (int i=0; i<10; i++){
+    println "sandeep-${i+1}"
+}
+
+def data = ["apple","grapes","Banana"]
+data.each{
+    println it
+}
+def data = ["apple","grapes","Banana"]
+data.each{   x->
+    println "food name is ${x}"
+}
+def data = ["apple","grapes","Banana"]
+for (x in data){
+    println("fruit name is ${x}")
+}
+
+10.times{
+    println "java Home ${it}"
+}
+
